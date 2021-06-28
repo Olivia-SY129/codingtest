562fd5e1016af27d93baf745890276dc41f6999c
STR21 <81205807+Olivia-SY129@users.noreply.github.com>
2021-06-29 00:26:31 +0900

    Update 7.py

--- codingtest/leetcode/7.py
+++ codingtest/leetcode/7.py
@@ -22,3 +22,30 @@ def twoSum2(nums: list[int], target: int) -> list[int]:
       return [i, nums[i + 1:].index(complement) + (i + 1)]
 
 print(twoSum2(nums, target))
+
+
+# 딕셔너리를 이용한 조회
+def twoSum3(nums: list[int], target: int) -> list[int]:
+  nums_map = {}
+  # 키와 값을 바꿔서 딕셔너리로 저장
+  for i, num in enumerate(nums):
+    nums_map[num] = i
+
+  # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+  for i, num in enumerate(nums):
+    if target - num in nums_map and i != nums_map[target - num]:
+      return [i, nums_map[target - num]]
+
+print(twoSum3(nums, target))
+
+
+# twoSum3 개선
+def twoSum4(nums: list[int], target: int) -> list[int]:
+  nums_map = {}
+  # 하나의 for 문으로 통합
+  for i, num in enumerate(nums):
+    if target - num in nums_map:
+      return [nums_map[target - num], i]
+    nums_map[num] = i
+
+print(twoSum4(nums, target))
