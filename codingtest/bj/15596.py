aa6b7adc090be16235438aa8c3a27bdb2cc5c208
STR21 <81205807+Olivia-SY129@users.noreply.github.com>
2021-07-02 22:51:50 +0900

    Update 15596.py

--- codingtest/bj/15596.py
+++ codingtest/bj/15596.py
@@ -1,2 +1,13 @@
-def sum(*numbers):
-  
+def solve(a: list) -> int:
+  sum = 0
+  for num in a:
+    sum += num
+  return sum
+
+import random
+
+lt = random.sample(range(100), 10)
+a = solve(lt)
+b = sum(lt)
+
+print(a, b)
