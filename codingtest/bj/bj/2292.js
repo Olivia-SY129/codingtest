fd4c3ab4ba493a674769248b7d8c61e5ca4b234a
STR21 <81205807+Olivia-SY129@users.noreply.github.com>
2021-07-10 22:37:58 +0900

    Update 2292.js

--- codingtest/bj/bj/2292.js
+++ codingtest/bj/bj/2292.js
@@ -4,12 +4,10 @@ const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
 i = 1;
 end = 1 + 3 * (i * (i - 1));
-// console.log(end);
 
 while (input > end) {
   i += 1;
   end = 1 + 3 * (i * (i - 1));
-  // console.log(end);
 }
 
 console.log(i);
